d85d30aa02c9bd1acd5398747203d35853397111
Lucasweiss <lukrassful@gmail.com>
2023-08-07 09:55:19 +0200

    change button color

--- frontend/app/components/AddToCartButton.tsx
+++ frontend/app/components/AddToCartButton.tsx
@@ -6,7 +6,7 @@ interface AddToCartButtonProps {
 export default function AddToCartButton (props: AddToCartButtonProps) {
   return (
     <button
-      className="bg-indigo-500 hover:bg-indigo-600 font-bold py-2 px-4 rounded focus:outline-none"
+      className="text-white bg-emerald-500 hover:bg-emerald-600 font-bold py-2 px-4 rounded focus:outline-none"
       onClick={e => {
         e.stopPropagation(); // prevent triggering card click
         props.onClick();
